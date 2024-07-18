1ae67b09f6480a231910d342e86d387c345d0558
Kartike Chopra <62751693+Scythektk@users.noreply.github.com>
2024-07-18 12:30:20 +0530

    Update addReport.tsx

--- addReport.tsx
+++ addReport.tsx
@@ -1,50 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-const ReportComponent = () => {
-  const [reports, setReports] = useState<{ id: number, reportUrl: string, reportName: string, modifyDate: string }[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/api/report');
-        setReports(response.data);
-      } catch (error) {
-        console.error('Error fetching reports:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h1>Reports</h1>
+<div>{date}</div>
+      <div>{documentId}</div>
+      
+      <h1>1. INTRODUCTION</h1>
+      
+      <h2>1.1. What is Digital Accessibility</h2>
+      <p>
+        Digital accessibility is about making communication services to the public online accessible to people with
+        disabilities by:
+      </p>
       <ul>
-        {reports.map((report, index) => (
-        //   <li key={report.id}>
-        //     <a href={report.reportUrl}>{report.reportName}</a> - Last Modified: {report.modifyDate}
-        //   </li>
-        <li key={report.id} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', border: '1px solid #ddd', borderRadius: '5px', margin: '10px 0' }}>
-          <div>
-            <h2>{report.reportName}</h2>
-            <p><a href={report.reportUrl}>Report URL</a></p>
-            <p>Page Names: {/* Assuming pageNames is an array and needs to be displayed */}</p>
-            <ul>
-              {report.pageNames.map((pageName, index) => (
-                <li key={index}>{pageName}</li>
-              ))}
-            </ul>
-            <p><a href={report.gridlink}>Grid Link</a></p>
-          </div>
-          <div style={{ textAlign: 'right' }}>
-            <p>Upload Date: {new Date(report.modifyDate).toLocaleDateString()}</p>
-          </div>
+        <li>
+          <strong>Perceptible:</strong> For example, facilitating the visual and auditory perception of content by the user;
+          offering textual equivalents to any non-textual content; creating content that can be presented in
+          different ways without loss of information or structure (for example, with a simplified layout).
         </li>
-        ))}
+        <li>
+          <strong>Usable:</strong> For example, providing the user with orientation elements to navigate, find the content;
+          making all the features accessible on the keyboard; giving the user enough time to read and use the
+          content; not conceive of content likely to cause epileptic seizures.
+        </li>
+        <li>
+          <strong>Understandable:</strong> For example, making pages work in a predictable way, helping the user to correct
+          input errors and
+        </li>
+        <li>
+          <strong>Robust:</strong> For example, optimizing compatibility with current and future uses, including assistive
+          technologies.
+        </li>
       </ul>
-    </div>
-  );
-};
-
-export default ReportComponent;
+      
+      <h2>1.2. Test Objective and Reference</h2>
+      <p>
+        The aim of this document is to evaluate the accessibility level of Société Générale – Application URL's
+        Application.
+      </p>
+      <p>
+        The standard used for the test is the RGAA version 4.1, published by the French government.
+        This reference document is based on international recommendations WCAG 2.1 (Web Content Accessibility
+        Guidelines) level AA.
+      </p>
+      <p>
+        It is fully compatible with WCAG 2.1 level AA, as well as with European standard EN 301 549 V2.1.2.
+        For all the criteria cited in the body of this test, a reference to the corresponding success criteria of the
+        WCAG 2.1 can be found on the official website of the RGAA:
+      </p>
+      <a href="https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/">
+        https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/
+      </a>
+      <p>
+        The RGAA version 4.1 reference document provides a method of applying and evaluating WCAG 2.1
+        international recommendations, more effective to implement during tests, and easier to use for the
+        people who will then have to take charge of the corrections.
+      </p>
